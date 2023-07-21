56cccf0c0c1dbc296ee09fefa664ded7e3e5a3f5
Gabriel Silva Mendes <97321099+g-smendes@users.noreply.github.com>
2023-07-20 22:05:17 -0300

    Add files via upload

---
+++ Results.tsx
@@ -0,0 +1,23 @@
+import { Questions } from "@/types/Question";
+
+type Props = {
+    questions: Questions[];
+    answers: number[];
+}
+
+export const Results = ({questions, answers}: Props) => {
+
+    return (
+        <div>
+            {questions.map((item, key) => (
+                <div key={key} className="mb-3">
+                    <div className="font-bold">{key + 1}. {item.questions}</div>
+                    <div>
+                        <span>({item.answer === answers[key] ? 'Acertou' : 'Errou'}) - </span>
+                        {item.options[item.answer]}
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
+}
